b7f6eca8345f8f871577b2e0a7960c1a2119d611
Pablo Rubiano <pcrubianoa@gmail.com>
2020-02-12 15:41:24 -0500

    WIP

--- index.php
+++ index.php
@@ -1,67 +1,67 @@
 <?php
 
-require 'vendor/autoload.php';
-
-echo "Archivo del formulario:";
-print_r($_FILES);
-
-echo "Document Root:";
-print_r($_SERVER['DOCUMENT_ROOT']);
-
-	function uploadImage($destino,$archivo) {
-		// Archivos permitidos y tamaño
-		$permitidos = array("image/jpg", "image/jpeg", "image/gif", "image/png");
-		$limite_kb = 10000000;
+		require 'vendor/autoload.php';
 		
-		if (in_array($archivo['type'], $permitidos) && $archivo['size'] <= $limite_kb * 1024){
-			// Ruta de destino
-			//$ruta = "../assets/images/" . $_FILES['imagen']['name'];
-			$name = date('mdyhis') . "-" . strtolower(str_replace(" ","-",$archivo['name']));
-			$ruta = $_SERVER['DOCUMENT_ROOT'].$destino ."/". $name;
-			//echo $ruta;
-			// Verificar si el archivo existe
-			if (!file_exists($ruta)){
-				//Se mueve el archivo a la carpeta de destino
-				$resultado = @move_uploaded_file($archivo["tmp_name"], $ruta);
-				if ($resultado){
-					//echo "El archivo ha sido movido exitosamente";
+		echo "Archivo del formulario:";
+		print_r($_FILES);
+		
+		echo "Document Root:";
+		print_r($_SERVER['DOCUMENT_ROOT']);
+		
+			function uploadImage($destino,$archivo) {
+				// Archivos permitidos y tamaño
+				$permitidos = array("image/jpg", "image/jpeg", "image/gif", "image/png");
+				$limite_kb = 10000000;
+				
+				if (in_array($archivo['type'], $permitidos) && $archivo['size'] <= $limite_kb * 1024){
+					// Ruta de destino
+					//$ruta = "../assets/images/" . $_FILES['imagen']['name'];
+					$name = date('mdyhis') . "-" . strtolower(str_replace(" ","-",$archivo['name']));
+					$ruta = $_SERVER['DOCUMENT_ROOT'].$destino ."/". $name;
+					//echo $ruta;
+					// Verificar si el archivo existe
+					if (!file_exists($ruta)){
+						//Se mueve el archivo a la carpeta de destino
+						$resultado = @move_uploaded_file($archivo["tmp_name"], $ruta);
+						if ($resultado){
+							//echo "El archivo ha sido movido exitosamente";
+						} else {
+							//echo "Ocurrio un error al mover el archivo.";
+						}
+					} else {
+						//echo $_FILES['imagen']['name'] . ", este archivo existe";
+						$resultado = @move_uploaded_file($archivo["tmp_name"], $ruta);
+					}
 				} else {
-					//echo "Ocurrio un error al mover el archivo.";
+					//echo "Archivo no permitido, es tipo de archivo prohibido o excede el tamano de $limite_kb Kilobytes";
 				}
-			} else {
-				//echo $_FILES['imagen']['name'] . ", este archivo existe";
-				$resultado = @move_uploaded_file($archivo["tmp_name"], $ruta);
+				return $name;
 			}
-		} else {
-			//echo "Archivo no permitido, es tipo de archivo prohibido o excede el tamano de $limite_kb Kilobytes";
+			
+			function deleteImage ($destino,$archivo) {
+				$ruta = $_SERVER['DOCUMENT_ROOT'].$destino ."/". $archivo;
+				//echo $ruta;
+				unlink($ruta);
+			}
+		
+		foreach ($_FILES as $key => $value) {
+			if ($value['name']!='') {
+				$nombre_imagen = uploadImage('/logis-images/cityu',$value);
+			}
 		}
-		return $name;
-	}
-	
-	function deleteImage ($destino,$archivo) {
-		$ruta = $_SERVER['DOCUMENT_ROOT'].$destino ."/". $archivo;
-		//echo $ruta;
-		unlink($ruta);
-	}
-
-foreach ($_FILES as $key => $value) {
-	if ($value['name']!='') {
-		$nombre_imagen = uploadImage('/logis-images/cityu',$value);
-	}
-}
-
-//API URL
-$url = 'http://35.231.168.139/logis-images/receiver.php';
-
-use GuzzleHttp\Client;
-
-$client = new \GuzzleHttp\Client(["base_uri" => "http://35.231.168.139"]);
-$options = [
-	'form_params' => [
-		$_POST,
-	]
-];
-$response = $client->post("/logis-images/receiver.php", $options);
-echo $response->getBody();
+		
+		//API URL
+		$url = 'http://35.231.168.139/logis-images/receiver.php';
+		
+		use GuzzleHttp\Client;
+		
+		$client = new \GuzzleHttp\Client(["base_uri" => "https://logis.com.co/app"]);
+		$options = [
+			'form_params' => [
+				$_POST,
+			]
+		];
+		$response = $client->post("/api/web/formulario_sorteo.php", $options);
+		echo $response->getBody();
 
 ?>
