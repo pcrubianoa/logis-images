f8d2fa532f70e666067f381a1ad958c9fc5b0c5c
Pablo Rubiano <pcrubianoa@gmail.com>
2020-02-12 10:58:57 -0500

    WIP

--- index.php
+++ index.php
@@ -45,31 +45,7 @@ foreach ($_FILES as $key => $value) {
 
 //API URL
 $url = 'http://35.231.168.139/logis-images/receiver.php';
-
-//create a new cURL resource
-$ch = curl_init($url);
-
-//setup request to send json via POST
-$data = array(
-	'username' => 'codexworld',
-	'password' => '123456'
-);
-$payload = json_encode(array("user" => $data));
-
-//attach encoded JSON string to the POST fields
-curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-
-//set the content type to application/json
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-
-//return response instead of outputting
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-//execute the POST request
-$result = curl_exec($ch);
-
-//close cURL resource
-curl_close($ch);
+echo "ok";
 
 /*
 $ch = curl_init();
