3622fb117f1963ffe6659658efd672690f2f2c0e
Pablo Rubiano <pcrubianoa@gmail.com>
2020-02-12 15:44:39 -0500

    WIP

--- index.php
+++ index.php
@@ -2,47 +2,41 @@
 
 		require 'vendor/autoload.php';
 		
-		echo "Archivo del formulario:";
-		print_r($_FILES);
-		
-		echo "Document Root:";
-		print_r($_SERVER['DOCUMENT_ROOT']);
-		
-			function uploadImage($destino,$archivo) {
-				// Archivos permitidos y tamaño
-				$permitidos = array("image/jpg", "image/jpeg", "image/gif", "image/png");
-				$limite_kb = 10000000;
-				
-				if (in_array($archivo['type'], $permitidos) && $archivo['size'] <= $limite_kb * 1024){
-					// Ruta de destino
-					//$ruta = "../assets/images/" . $_FILES['imagen']['name'];
-					$name = date('mdyhis') . "-" . strtolower(str_replace(" ","-",$archivo['name']));
-					$ruta = $_SERVER['DOCUMENT_ROOT'].$destino ."/". $name;
-					//echo $ruta;
-					// Verificar si el archivo existe
-					if (!file_exists($ruta)){
-						//Se mueve el archivo a la carpeta de destino
-						$resultado = @move_uploaded_file($archivo["tmp_name"], $ruta);
-						if ($resultado){
-							//echo "El archivo ha sido movido exitosamente";
-						} else {
-							//echo "Ocurrio un error al mover el archivo.";
-						}
+		function uploadImage($destino,$archivo) {
+			// Archivos permitidos y tamaño
+			$permitidos = array("image/jpg", "image/jpeg", "image/gif", "image/png");
+			$limite_kb = 10000000;
+			
+			if (in_array($archivo['type'], $permitidos) && $archivo['size'] <= $limite_kb * 1024){
+				// Ruta de destino
+				//$ruta = "../assets/images/" . $_FILES['imagen']['name'];
+				$name = date('mdyhis') . "-" . strtolower(str_replace(" ","-",$archivo['name']));
+				$ruta = $_SERVER['DOCUMENT_ROOT'].$destino ."/". $name;
+				//echo $ruta;
+				// Verificar si el archivo existe
+				if (!file_exists($ruta)){
+					//Se mueve el archivo a la carpeta de destino
+					$resultado = @move_uploaded_file($archivo["tmp_name"], $ruta);
+					if ($resultado){
+						//echo "El archivo ha sido movido exitosamente";
 					} else {
-						//echo $_FILES['imagen']['name'] . ", este archivo existe";
-						$resultado = @move_uploaded_file($archivo["tmp_name"], $ruta);
+						//echo "Ocurrio un error al mover el archivo.";
 					}
 				} else {
-					//echo "Archivo no permitido, es tipo de archivo prohibido o excede el tamano de $limite_kb Kilobytes";
+					//echo $_FILES['imagen']['name'] . ", este archivo existe";
+					$resultado = @move_uploaded_file($archivo["tmp_name"], $ruta);
 				}
-				return $name;
+			} else {
+				//echo "Archivo no permitido, es tipo de archivo prohibido o excede el tamano de $limite_kb Kilobytes";
 			}
+			return $name;
+		}
 			
-			function deleteImage ($destino,$archivo) {
-				$ruta = $_SERVER['DOCUMENT_ROOT'].$destino ."/". $archivo;
-				//echo $ruta;
-				unlink($ruta);
-			}
+		function deleteImage ($destino,$archivo) {
+			$ruta = $_SERVER['DOCUMENT_ROOT'].$destino ."/". $archivo;
+			//echo $ruta;
+			unlink($ruta);
+		}
 		
 		foreach ($_FILES as $key => $value) {
 			if ($value['name']!='') {
@@ -55,13 +49,13 @@
 		
 		use GuzzleHttp\Client;
 		
-		$client = new \GuzzleHttp\Client(["base_uri" => "https://logis.com.co/app"]);
+		$client = new \GuzzleHttp\Client(["base_uri" => "https://logis.com.co"]);
 		$options = [
 			'form_params' => [
 				$_POST,
 			]
 		];
-		$response = $client->post("/api/web/formulario_sorteo.php", $options);
+		$response = $client->post("/app/api/web/formulario_sorteo.php", $options);
 		echo $response->getBody();
 
 ?>
