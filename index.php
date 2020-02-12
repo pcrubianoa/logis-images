b1c5fb89dbc37049cd366d9d264a4b95b8c9e178
Pablo Rubiano <pcrubianoa@gmail.com>
2020-02-11 20:50:06 -0500

    WIP

--- index.php
+++ index.php
@@ -10,29 +10,29 @@
 			//$ruta = "../assets/images/" . $_FILES['imagen']['name'];
 			$name = date('mdyhis') . "-" . strtolower(str_replace(" ","-",$archivo['name']));
 			$ruta = $_SERVER['DOCUMENT_ROOT'].$destino ."/". $name;
-			echo $ruta;
+			//echo $ruta;
 			// Verificar si el archivo existe
 			if (!file_exists($ruta)){
 				//Se mueve el archivo a la carpeta de destino
 				$resultado = @move_uploaded_file($archivo["tmp_name"], $ruta);
 				if ($resultado){
-					echo "El archivo ha sido movido exitosamente";
+					//echo "El archivo ha sido movido exitosamente";
 				} else {
-					echo "Ocurrio un error al mover el archivo.";
+					//echo "Ocurrio un error al mover el archivo.";
 				}
 			} else {
-				echo $_FILES['imagen']['name'] . ", este archivo existe";
+				//echo $_FILES['imagen']['name'] . ", este archivo existe";
 				$resultado = @move_uploaded_file($archivo["tmp_name"], $ruta);
 			}
 		} else {
-			echo "Archivo no permitido, es tipo de archivo prohibido o excede el tamano de $limite_kb Kilobytes";
+			//echo "Archivo no permitido, es tipo de archivo prohibido o excede el tamano de $limite_kb Kilobytes";
 		}
 		return $name;
 	}
 	
 	function deleteImage ($destino,$archivo) {
 		$ruta = $_SERVER['DOCUMENT_ROOT'].$destino ."/". $archivo;
-		echo $ruta;
+		//echo $ruta;
 		unlink($ruta);
 	}
 
@@ -41,9 +41,16 @@ foreach ($_FILES as $key => $value) {
 		$nombre_imagen = uploadImage('/logis-images/cityu',$value);
 	}
 }
-	
-	print_r($_FILES);
-	echo '<hr>';
-	print_r($_POST);
+
+
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://logis.com.co/api/web/formulario_sorteo.php?empresa=dev");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($_POST));
+
+$result = curl_exec ($ch);
+$res = json_decode($result);
 
 ?>
