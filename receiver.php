c239387506e5171d53c9b9989b8918da123e4666
Pablo Rubiano <pcrubianoa@gmail.com>
2020-02-12 11:07:12 -0500

    WIP

--- receiver.php
+++ receiver.php
@@ -1,7 +1,7 @@
 <?php
-
-// get the raw POST data
-$rawData = file_get_contents("php://input");
-
-// this returns null if not valid json
-return json_decode($rawData, true);  // will return array
+	
+	// get the raw POST data
+	$rawData = file_get_contents("php://input");
+	
+	// this returns null if not valid json
+	return json_decode($rawData, true);  // will return array
